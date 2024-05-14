04624b89dbbe5f679b2877dfe5a9250e3c7166cf
Dragon <abolfazl.asadi20asli@gmail.com>
2024-05-14 21:50:26 +0330

    Update _worker.js

--- Vless_workers_pages/_worker.js
+++ Vless_workers_pages/_worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = '77a571fb-4fd2-4b37-8596-1b7d9728bb5c';
 
 const proxyIPs = ["cdn.xn--b6gac.eu.org"]; //workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
-const cn_hostnames = [''];
+const cn_hostnames = ['speedtest.net'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
